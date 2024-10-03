add5090f69c8aecfee949bda1a827c0b3569aacd
Matvey133 <mateyhcerevatov9@gmail.com>
2024-10-03 22:23:35 +0300

    Add files via upload

---
+++ LabaPeremen.py
@@ -0,0 +1,5 @@
+The_number_of_completed_DZ=12
+The_number_of_hours_spent=1.5
+Course_name="Python"
+Time_for_one_task=The_number_of_hours_spent/The_number_of_completed_DZ
+print("Курс:",Course_name,",","всего задач:",The_number_of_completed_DZ,",","затрачено часов:",The_number_of_hours_spent,",","среднее время выполнения",Time_for_one_task,"часа",".")
